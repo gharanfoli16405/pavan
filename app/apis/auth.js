b1024bc7910090796faefa3844877681d24bf1dd
Gharanfoli16045 <frontenddeveloper16405@gmail.com>
2025-02-08 05:37:47 -0800

    accdess token

--- app/apis/auth.js
+++ app/apis/auth.js
@@ -20,3 +20,33 @@ const refreshToken = async () => {
 };
 
 export { login, forgetPassword, refreshToken };
+
+if (error.response.status === 401 && !originalRequest._retry) {
+  originalRequest._retry = true;
+  try {
+    const refreshToken = await getRefreshToken();
+    const response = await refreshTokenApi({ refreshToken: refreshToken });
+
+    const newAccessToken = response?.data?.access_token;
+    const newRefreshToken = response?.data?.refresh_token;
+
+    const lastCookie = await getCookie("credential");
+    const newCookie = {
+      ...lastCookie,
+      access_token: newAccessToken,
+      refresh_token: newRefreshToken,
+    };
+
+    console.log("new credential is \n:", newCookie);
+    await setCookie("credential", newCookie);
+
+    apiClient.defaults.headers["Authorization"] = `Bearer ${newAccessToken}`;
+
+    originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+
+    return apiClient(originalRequest);
+  } catch (error) {
+    console.log("failed to submit new refresh token!");
+    return Promise.reject(error);
+  }
+}
